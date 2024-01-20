1da13d81c416731dcf73f31b803918f123bf054c
wildone <max.barrass@gmail.com>
2024-01-20 19:32:22 +1100

    allow compiling of additional preload scripts.

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import inject from '@rollup/plugin-inject'
+import { resolve } from 'path'
 
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin(), inject({ $: 'jquery', jQuery: 'jquery' })]
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'src/preload/index.ts'),
+          trusted: resolve(__dirname, 'src/preload/trusted.ts')
+        }
+      }
+    }
   },
   renderer: {
     server: {
